a8fa2ef08e689cd016678830d8856981357bca5a
INDIAN CYBER FORCE <95233999+SanatNayak@users.noreply.github.com>
2022-11-26 22:07:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>INDIAN CYBER FORCE</title>
+<script>alert("HACKED BY MR. GR3Y H4CK3R")</script>
+    <link href='https://fonts.googleapis.com/css?family=Kanit' rel='stylesheet' type='text/css' />
+    <link href='https://fonts.googleapis.com/css?family=Share+Tech:400' rel='stylesheet' type='text/css' />
+    <link href='https://fonts.googleapis.com/css?family=Cinzel:400' rel='stylesheet' type='text/css' />
+
+	<style type="text/css">
+@import url('https://fonts.googleapis.com/css?family=Lobster&display=swap') repeat scroll 0 0 rgba(0, 0, 0 , 0);
+
+@font-face { font-family: DaysLater; src: url('https://d4rk-vamp1re.github.io/js-host/28DaysLater-MA0x.ttf'); } 
+ .ict-log{
+         font-family: DaysLater;
+         text-align: center;
+      }
+
+body { background-color: #000;
+ }
+
+.ict-log{
+  font-size: 36px;
+     text-shadow: 0 0 10px #000;
+     text-align: center;
+}
+
+   .vamp{
+   text-shadow: 0 0 10px #7200E6;
+   color: white;
+   } 
+		img:hover{
+             animation: shake 1s;
+  			 animation-iteration-count: infinite;
+		}
+
+      @keyframes shake {
+           0% { transform: translate(1px, 1px) rotate(0deg); }
+           10% { transform: translate(-1px, -2px) rotate(-1deg); }
+           20% { transform: translate(-2px, 0px) rotate(1deg); }
+           30% { transform: translate(2px, 2px) rotate(0deg); }
+           40% { transform: translate(1px, -1px) rotate(1deg); }
+           50% { transform: translate(-1px, 2px) rotate(-1deg); }
+           60% { transform: translate(-2px, 1px) rotate(0deg); }
+           70% { transform: translate(2px, 1px) rotate(-1deg); }
+           80% { transform: translate(-1px, -1px) rotate(1deg); }
+           90% { transform: translate(1px, 2px) rotate(0deg); }
+           100% { transform: translate(1px, -2px) rotate(-1deg); }
+         }
+
+            .blueglow {
+   text-shadow: 0 0 10px #74E600;
+   color: white;
+   font-family: "Kanit";
+   } 
+        .blink{
+      animation: blinker 0.6s linear infinite;
+      font-size: 25px;
+      color:  #f00000;
+      font-weight: bold;
+      text-align: center;
+      font-family: sans-serif;
+    }
+              @keyframes blinker {  
+      50% { opacity: 0; }
+      }
+
+      @font-face { font-family: FinaFont; src: url('FinalFrontierOldStyle-8Pg.ttf'); } 
+ .vamp{
+         font-family: "Kanit";
+         text-align: center;
+      }
+.legion{
+	color: white;
+	font-weight: bold;
+	font-size: 14px;
+	font-family: "Kanit";
+}
+      .team{
+        text-align: center;
+        font-family: "Share Tech";
+        font-size: 18px;
+      }
+	</style>
+</head>
+<body>
+
+	<script type="text/javascript" src="https://aakashbapna.github.io/jet-planes/doodle.js"></script>
+  <script type="text/javascript">
+  doodle.init("http://aakashbapna.github.io/jet-planes/jet.png");
+  </script>
+
+ <div class="animated flip">
+ <center>
+  <span>
+ <img src="https://i.imgur.com/73CPcUS.jpg" height="280" width="280"> 
+ </span>
+</div>   
+ </center>
+<center><font <br> <h1><font face="Iceland" style="font-family: 'Copperplate', sans-serif;" color="#FFA500">INDIAN</font> <font face="Iceland" style="font-family: 'Copperplate', sans-serif;" color="white">CYBER</font> <font face="Iceland" style="font-family: 'Copperplate', sans-serif;" color="#32CD32">FORCE</font></h1> 
+<center>
+ <p class="legion">//-[ Mr.Grey Hacker Was Here ]-\\</p>
+<!--<img src="https://i.ibb.co/7tw13Xd/plain-flag.jpg" alt="plain-flag" width="100%" border="0">
+  <p class="ict-log"><font color="#FFA500">Indian</font> <font color="white">Cyber</font> <font color="#32CD32">Ninjas</font></p>-->
+  
+  </center>
+</div>
+<center>
+<p style="font-weight: bold; font-family: sans-serif; font-size: 25px;"><font color="white">[</font><font class="blink" color="blue">Hacked By Mr.Grey Hacker</font><font color="white">]</font></
+</center>
+<center>
+<span class="blueglow" style="font-size: 17px; text-align: center;"> Hacked By I.C.F Team <br>  
+</span>
+</center>
+<br><br>
+	<span>
+		<center><p class=team><font size="5" style="color: red;">🖕 F*CKED YOUR SYSTEM 🖕</font><br>
+		</span></center>5
+		<br>
+		<audio autoplay controls>
+  <source src="https://xmr4n0nx.github.io/Kadam Kadam Badhaye Ja.mp3" type="audio/mpeg">
+  <source src="/media/epic.wav" type="audio/wav">
+</audio>
+			<br><br>
+<center> <img alt="indian-flag" src="https://bestanimations.com/media/india/600736555india-flag-waving-animated-gif-2.gif" height="200px" width="300px"></center> <center><a href="https://t.me/INDIAN_CYBER_FORCE"><font face="kalam" color="aqua" size="2">Telegram Channel</font></a></center> <br> <h2>~°./ 4LPH4 3V1L \.°~</h2>
+<span>
+	<p class="team"><font size="4" style="color: white;"> :: GREETZ TO ::</font><br><marquee>
+  <font color="white">[</font><font color="red"> |Mr.Grey Hacker |SHELL_INSERTER⁩ |BlackHaxSEC |Mr.Virus |P4nthom | Ajax Walter |C¥B€R R€AP€R |Anonymous |Sayed |David</font><font color="white">]</font></marquee>
+</p>
+</span>
+
+    <script type="text/javascript">
+var rev = "silent";
+function titlebar(val)
+{
+    var msg  = "//--[ HACKED BY Mr. Grey Hacker]";
+    var res = " ";
+    var speed = 70
+    var pos = val;
+
+    msg = ""+msg+"";
+    var le = msg.length;
+    if(rev == "silent"){
+        if(pos < le){
+        pos = pos+1;
+        scroll = msg.substr(0,pos);
+        document.title = scroll;
+        timer = window.setTimeout("titlebar("+pos+")",speed);
+        }
+        else{
+        rev = "silents";
+        timer = window.setTimeout("titlebar("+pos+")",speed);
+        }
+    }
+    else{
+        if(pos > 0){
+        pos = pos-1;
+        var ale = le-pos;
+        scrol = msg.substr(ale,le);
+        document.title = scrol;
+        timer = window.setTimeout("titlebar("+pos+")",speed);
+        }
+        else{
+        rev = "silent";
+        timer = window.setTimeout("titlebar("+pos+")",speed);
+        }   
+    }
+}
+
+titlebar(0);
+</script>
+
+<script type="text/javascript">
+function disableselect(e) {
+return false
+}
+function reEnable() {
+return true
+}
+document.onselectstart = new Function("return false")
+if (window.sidebar) {
+document.onmousedown = disableselect
+document.onclick = reEnable
+}
+var message = "";
+function clickIE() {
+if (document.all) {
+(message);
+return false;
+}
+}
+function clickNS(e) {
+if (document.layers || (document.getElementById && !document.all)) {
+if (e.which == 2 || e.which == 3) {
+(message);
+return false;
+}
+}
+}
+if (document.layers) {
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown = clickNS;
+} else {
+document.onmouseup = clickNS;
+document.oncontextmenu = clickIE;
+}
+document.oncontextmenu = new Function('alert("HACKED BY Indian Cyber Force !!"); return false')	</style>
+</body> </head> </html>
